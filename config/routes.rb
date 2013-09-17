3a6ea453cd3edc5949868f08c20ca448a6b8786a
Marek Mikołajczyk <deashay@gmail.com>
2013-09-17 22:20:09 +0200

    Adding active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 CodenameOne::Application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root to: 'home#index'
+
+  resources :users
+
+  resources :races
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 end
