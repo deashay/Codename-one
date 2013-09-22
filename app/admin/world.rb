1155c19702febd389a55f3299125c79a4c4456fa
Marek Mikołajczyk <deashay@gmail.com>
2013-09-22 18:21:09 +0200

    Changes in admin panel for world

--- app/admin/world.rb
+++ app/admin/world.rb
@@ -1,12 +1,16 @@
 ActiveAdmin.register World do     
   index do                            
-    column :name          
+    column :name    
+    column :width
+    column :height      
     default_actions                   
   end                                                  
 
   form do |f|                         
     f.inputs "World Details" do       
-      f.input :name                  
+      f.input :name  
+      f.input :width, as: :number
+      f.input :height, as: :number               
     end                               
     f.actions                         
   end                                 
