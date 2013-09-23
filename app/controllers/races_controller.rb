9ce31a2eac717aa50067ea5d08edfac03e6aa636
Marek Mikołajczyk <deashay@gmail.com>
2013-09-23 19:10:13 +0200

    Adding activation to race

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,6 +1,8 @@
 class RacesController < ApplicationController
   expose(:race)
 
+  before_filter :check_users_world_existance, only: :activate
+
   def create
     if race.save
       current_user.races << race
@@ -8,5 +10,17 @@ class RacesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def activate  
+    current_user.races.where(:id.ne => race.id).update_all(active: false)
+    race.update_attribute(:active, true)
+    redirect_to root_path
+  end
+
+  protected
+
+  def check_users_world_existance
+    redirect_to root_path if current_user.world.present?
   end
 end
