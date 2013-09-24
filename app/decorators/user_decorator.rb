d455721e9cf196acbc5bbdbfe09a0c16ea782169
nekath <radoslaw.piatek@netguru.pl>
2013-09-24 23:52:25 +0200

    syntax fix

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,6 +2,6 @@ class UserDecorator < Draper::Decorator
   delegate_all
 
   def world
-    object.world || "You are not connected to any world"
+    object.world || 'You are not connected to any world'
   end
 end
