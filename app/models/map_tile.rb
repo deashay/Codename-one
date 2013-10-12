49a23148bb4017338893c751aa951fa53122e991
piasekhgw <wojciech.piasecki@netguru.pl>
2013-10-12 21:00:05 +0200

    [#58301504] x, y in model

--- app/models/map_tile.rb
+++ app/models/map_tile.rb
@@ -1,10 +1,13 @@
 class MapTile
   include Mongoid::Document
 
+  field :x, type: Integer
+  field :y, type: Integer
+
   belongs_to :world
   belongs_to :influencing_city, class_name: 'City'
   belongs_to :country, inverse_of: :lands
-  
+
   has_many :armies
   has_many :cities
   has_many :buildings
