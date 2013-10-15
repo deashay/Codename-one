86dcb5c3f43fca54102d30c145575320ec972886
nekath <radoslaw.piatek@netguru.pl>
2013-10-15 21:02:02 +0200

    typos, typo
    [#58301200]

--- app/models/army.rb
+++ app/models/army.rb
@@ -1,7 +1,7 @@
 class Army
   include Mongoid::Document
 
-  after_save :recalculate_max_weigth_of_resources
+  after_save :recalculate_max_weight_of_resources
 
   belongs_to :map_tile
   belongs_to :country
@@ -15,7 +15,7 @@ class Army
   field :max_weight, type: Float, default: 0.0
 
   validates :name, presence: true, uniqueness: true
-  validates :max_weigth, numericality: { greater_than_or_equal_to: 0 }
+  validates :max_weight, numericality: { greater_than_or_equal_to: 0 }
   validates :race, presence: true
 
   delegate :world, to: :country
@@ -23,9 +23,9 @@ class Army
   private
 
   # keep in mind that race stats will affect group.
-  def recalculate_max_weigth_of_resources
+  def recalculate_max_weight_of_resources
     weight_attribute_id = world.config(:weight_attribute_id).weight_attribute_id
-    max_weigth_for_army = regiments.groups.map do |group|
+    max_weight_for_army = regiments.groups.map do |group|
       group.count * group.stats.of_kind(weight_attribute_id).first.value
     end.sum
     self.update_attributes(:max_weight, max_weight_for_army)
