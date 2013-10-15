82d49cac043a03ffafa8ec006dd06fa0524bbe86
nekath <radoslaw.piatek@netguru.pl>
2013-10-15 20:57:35 +0200

    Fix typo
    [#58301200]

--- app/models/army.rb
+++ app/models/army.rb
@@ -12,7 +12,7 @@ class Army
   has_many :resources, as: :acquired_resources
 
   field :name, type: String
-  field :max_weigth, type: Float, default: 0.0
+  field :max_weight, type: Float, default: 0.0
 
   validates :name, presence: true, uniqueness: true
   validates :max_weigth, numericality: { greater_than_or_equal_to: 0 }
@@ -24,10 +24,10 @@ class Army
 
   # keep in mind that race stats will affect group.
   def recalculate_max_weigth_of_resources
-    weigth_attribute_id = world.config(:weigth_attribute_id).weigth_attribute_id
+    weight_attribute_id = world.config(:weight_attribute_id).weight_attribute_id
     max_weigth_for_army = regiments.groups.map do |group|
-      group.count * group.stats.of_kind(weigth_attribute_id).first.value
+      group.count * group.stats.of_kind(weight_attribute_id).first.value
     end.sum
-    self.update_attributes(:can_lift, weigth_for_army)
+    self.update_attributes(:max_weight, max_weight_for_army)
   end
 end
